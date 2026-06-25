486aab4d39d8c45dce3eb81542fb9cc88349520f
jan657-567 <jgarg6970@gmail.com>
2026-06-26 00:21:59 +0530

    updates of app

---
+++ mydjangoapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def homepage(request):
+    return render(request,'home.html')
+def aboutpage(request):
+    return render(request,'about.html')
+def gallarypage(request):
+    return render(request,'gallary.html')
+def contactpage(request):
+    return render(request,'contact.html')
